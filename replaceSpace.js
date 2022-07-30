77a359bef898748b1d5014602839807069391b46
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 14:06:52 -0700

    initial commit

---
+++ replaceSpace.js
@@ -0,0 +1,10 @@
+// Question #1: Turning Strings to URLs
+
+// URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
+
+// You may not use the replace() method or regular expressions to solve this problem. Solve the problem with and without recursion.
+// Example
+
+// Input: "Jasmine Ann Jones"
+
+// Output: "Jasmine%20Ann%20Jones"
