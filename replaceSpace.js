7fd92a322cf6f4f3506e465816fbdc32a70d7959
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 14:09:20 -0700

    add turning strings to URLs solution

--- replaceSpace.js
+++ replaceSpace.js
@@ -8,3 +8,35 @@
 // Input: "Jasmine Ann Jones"
 
 // Output: "Jasmine%20Ann%20Jones"
+
+// notes:
+  //  for loop to interate through the string, look for spaces
+  // need to replace spaces with %
+  //  push into empty array??
+  //  how to replace empty spaces with %??
+
+// answer:
+  // use split() to grab the spaces, join() to replace spaces w/ %
+
+replaceSpace = (string) => {
+  let result = string.split(" ").join("%"); // replaces spaces with %
+  return result;
+};
+
+// console.log(replaceSpace("Jasmine Ann Jones"));
+
+// --- recursive function ---
+  // WIP
+  // getting the correct answer however, can't figure out termination case.
+  // creates infinite loop
+replaceSpaceRecursive = (string) => {
+  if (string < string.length) {
+    return string;
+  } else {
+    let result = string.split(" ").join("%");
+    console.log(`${result}`);
+    return replaceSpaceRecursive(result);
+  }
+};
+
+console.log(replaceSpaceRecursive("Jasmine Ann Jones"));
