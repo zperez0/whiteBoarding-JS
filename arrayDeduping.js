8ef4fa6bf63ce6a4d414f93b0600075959f04f40
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 14:16:22 -0700

    initial commit

---
+++ arrayDeduping.js
@@ -0,0 +1,9 @@
+// Question #2: Array Deduping
+
+// Write an algorithm that removes duplicates from an array. Do not use a function like filter() to solve this. Once you have solved the problem, demonstrate how it can be solved with filter(). Solve the problem with and without recursion.
+// Example
+
+// Input: [7, 9, "hi", 12, "hi" 7, 53]
+
+// Output: [7, 9, "hi", 12, 53]
+
