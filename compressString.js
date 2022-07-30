209d6feb410164014bda54574216974330520a2b
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 14:38:18 -0700

    add compressing strings solutions

--- compressString.js
+++ compressString.js
@@ -7,3 +7,65 @@
 
 // Output: "3ab2c4da"
 
+// notes:
+
+  // split string into an array -> so one can easily iterate over the string
+  // current count for the current letter
+  // while loop -> when conditions are met ( ) , will increase count & index position. will only move to the next letter when the condition is met.
+  // concat chars & count to empty str (results)
+
+compressString = (str) => {
+  let results = "";
+  let strArray = str.split("");
+  for (let i = 0; i < strArray.length; i++) {
+    let count = 1;
+    let currentChar = strArray[i];
+    while (i < strArray.length - 1 && strArray[i] === strArray[i + 1]) {
+      count++ && i++;
+    }
+    if (count === 1) {
+      results += currentChar;
+    } else {
+      results += currentChar + count;
+    }
+  }
+  return results;
+};
+
+// console.log(compressString("aaabccdddda"));
+
+
+// --- recursive function ---
+  // WIP
+
+// compressString = (str) => {
+//   let results = "";
+//   let strArray = str.split("");
+//   if( str > strArray.length){
+//     return str;
+//   } else {
+
+// };
+
+
+//first attempt
+
+// for loop -> iterate over the string
+// compare each character
+// if both characters are the same -> increment the count by 1 & concat string
+// use regex?
+
+// WIP -> creates infinite loop -__-
+
+// compressString = (str) => {
+//   let count = 0;
+//     for (let i=0; i < str.length; i++) {
+//       count++ && i++;
+//       if (str[i] != str[i+1]) {
+//         str += str[i] + count;
+//         console.log(`str: ${str}`);
+//       }
+//     }
+// }
+
+// console.log(compressString("aaabccdddda"));
