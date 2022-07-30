c1d7632d666ff7b57af58478a561e39391ef28af
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 15:13:50 -0700

    initial commit

---
+++ uniqueChar.js
@@ -0,0 +1,13 @@
+// Question #4: Checking for Uniqueness
+
+// Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an array or use array methods to solve this problem. The algorithm should return a boolean.
+// Example
+
+// Input: "hello"
+
+// Output: false
+
+// Input: "copyright"
+
+// Output: true
+
