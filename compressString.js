a29e84605b2a90b187d0f363ad76424af6f37ed2
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 14:21:16 -0700

    initial commit

---
+++ compressString.js
@@ -0,0 +1,9 @@
+// Question #3: Compressing Strings
+
+// Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
+// Example
+
+// Input: "aaabccdddda"
+
+// Output: "3ab2c4da"
+
