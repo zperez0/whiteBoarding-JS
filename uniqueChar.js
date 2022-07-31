8b620558be97859933f1909bf21271e74925b189
Yazmin Perez <zoeperez333@gmail.com>
2022-07-30 19:09:04 -0700

    add checking for uniqueness solutions

--- uniqueChar.js
+++ uniqueChar.js
@@ -11,3 +11,39 @@
 
 // Output: true
 
+// notes:
+  // find unique element from a str
+  // return true or false
+  // 2 for loops
+  // compare each character -> if characters match return false, if not return true
+
+uniqueChar = (str) => {
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// console.log(uniqueChar("hello")); // false
+// console.log(uniqueChar("copyright")); // true
+
+// --- recursive function ---
+
+  // WIP
+
+uniqueCharRecursive = (str, i) => {
+  if (str.length - i === 0) {
+    return;
+  } else {
+    return (
+      uniqueCharRecursive(str, i + 1) || str.substring(0, i).indexOf(str[i]) != -1
+    );
+  }
+};
+
+console.log(uniqueCharRecursive("hello")); // false
+console.log(uniqueCharRecursive("copyright")); // true
